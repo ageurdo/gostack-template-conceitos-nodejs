60cda19679fcfc28cec84652fadd469b0f4d7c7c
Ageu Rodrigues de Oliveira <ageurdo@gmail.com>
2020-06-17 08:57:50 -0300

    First backend challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
+const { json } = require("express");
+
+
 
 const app = express();
 
@@ -10,24 +13,69 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0,
+  };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (request, response) => {
+  console.log(repositories);
+  if (repositories < 1) {
+    return response.status(204).json("No content found!")
+  } else
+    return response.json(repositories);
 });
+
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const indexfound = repositories.findIndex((item) => item.id === id);
+  likes = repositories[indexfound].likes;
+
+  if (indexfound < 0) {
+    return response.status(404).json("Object not found!")
+  } else {
+    const updatedRepository = { id, title, url, techs, likes };
+    repositories[indexfound] = updatedRepository;
+    return response.status(204).send();
+  }
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const indexfound = repositories.findIndex((item) => item.id === id);
+
+  if (indexfound < 0) {
+    return response.status(404).json("Object not found!")
+  } else {
+    repositories.splice(indexfound, 1);
+    return response.status(204).json();
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const indexfound = repositories.findIndex((item) => item.id === id);
+
+  if (indexfound < 0) {
+    return response.status(404).json("Object not found!")
+  } else {
+    likeSum = repositories[indexfound].likes + 1;
+    repositories[indexfound].likes = likeSum;
+    return response.status(202).json(repositories[indexfound]);
+  }
+
+
 });
 
 module.exports = app;
